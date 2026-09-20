7af2fc83f5ccd991d9edca5c05daddb33af907fa
Isaiah <isaiahadepegba@gmail.com>
2026-09-20 15:55:04 +0100

    random & number guessing game

---
+++ import-random.py
@@ -0,0 +1,28 @@
+import random
+
+# low = 1
+# high = 100
+
+# number = random.randint(low, high)
+
+# print(number)
+
+############
+# number = random.random()
+# print(number)
+
+
+#######################
+# options = ("rock", "paper", "scissors")
+
+# option = random.choice(options)
+
+# print(option)
+
+
+######################
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+random.shuffle(cards)
+
+print(cards)
